2d597b17c7b065fd1e89286116ec1076525a4a98
Abhijit Pattnaik <abhijitbrain@gmail.com>
2021-05-02 20:05:05 +0530

    Switch case updates

--- switch2.c
+++ switch2.c
@@ -2,11 +2,12 @@
 
 void main()
 {
-    int choice;
-    int fact,n;
+    int loop =1;
+    int choice,fact,n;
+    int numb;
 
-    while(1){
-        printf("1: Factorial \n 2: Prime\n 3: Odd/Even\n 4: Exit \n");
+    while(loop){
+        printf("\n 1: Factorial \n 2: Prime\n 3: Odd/Even\n 4: Exit \n");
         printf("Your Choice : ");
         scanf("%d",&choice);
 
@@ -24,8 +25,33 @@ void main()
             printf("Factorial of %d is %d\n",n,fact);
             break;
         case 2:
-                
-                break;
+            printf("Prime or Not, Enter number : ");
+            scanf("%d",&numb);
+
+            int i,flag=0;
+            for(i=2;i<=numb/2;i++){
+                if(numb%i == 0)
+                    flag = 1;
+            }
+            if(flag == 1){
+                printf("Number is not PRIME \n");
+            }else{
+                printf("Number is PRIME \n");
+            }
+
+            break;
+        case 3:
+            printf("ODD/EVEN, Enter a number : ");
+            scanf("%d",&numb);
+
+            if(numb%2 == 0)
+                printf("Number is EVEN \n");
+            else
+                printf("Number is ODD \n");
+            break;
+        case 4:
+            loop=0;
+            break;
         default:
             break;
         }
